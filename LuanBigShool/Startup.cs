fdcbc83aafcb68b6333f7f30d3702f79a81a1a72
LeMinhLuan <leluansal98@gmail.com>
2019-04-11 10:43:30 +0700

    Add project files.

---
+++ LuanBigShool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LuanBigShool.Startup))]
+namespace LuanBigShool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
